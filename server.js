1a97720ccd1b77408db920f42ed1f7ee512eb5de
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-05-18 14:51:23 -0500

    for misti

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
+'use strict';
+
+let data = require('./data/weather.json');
+
+// this library lets us access our .env file
+require('dotenv').config();
+
+// express is a server library
 const express = require('express');
 
-const PORT = process.env.PORT || 5000
-
+// initalizes the express library
 const app = express();
 
+// library that determines who is allowed to speak to our server
+const cors = require('cors');
 
-app.get('/weather', (req,res)=>{
-    res.json({ express: req.query.city})
-})
+// this settting says that everyone is allowed to speak to our server
+app.use(cors());
+
+// we are getting the port variable from the .env file.
+const PORT = process.env.PORT;
+
+app.get('/weather', (request, response) => {
+	console.log({ data: request.query.city});
+	console.log({ express: request.query.city });
+	response.send({ express: request.query.city });
+});
+
+app.get('/', (request, response) => {
+    response.send({express: request.query.city})
+});
 
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+	console.log(`Server listening on ${PORT}`);
 });
