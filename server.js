a258b89e0d34fa75bff122df65d1e34aec19647c
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-05-19 16:28:15 -0500

    installed axios

--- server.js
+++ server.js
@@ -6,56 +6,36 @@ const cors = require('cors');
 app.use(cors());
 const PORT = process.env.PORT || 5001;
 let weatherData = require('./data/weather.json');
+import axios from 'axios';
 
 app.get('/weather', handleWeather);
 
+function handleWeather(request, response) {
+	console.log('server side ', request.query);
+	let searchQuery = request.query.city;
+	console.log('server console.log', searchQuery);
 
-function handleWeather(request, response){
-    console.log('server side ',request.query);
-        let searchQuery = request.query.city;
-        console.log('server console.log', searchQuery);
+	//get weather data
+	const cityWeather = weatherData.find(
+		(city) => city.city_name.toLowerCase() === searchQuery.toLowerCase()
+	);
+	console.log('cityWeather', cityWeather.data);
 
-        //get weather data
-        const cityWeather = weatherData.find(city => city.city_name.toLowerCase() === searchQuery.toLowerCase());
-        console.log('cityWeather', cityWeather.data);
+	//loop over weather data call constructor for building object from data.
+	const weatherArray = cityWeather.data.map((day) => new Forecast(day));
 
-        //loop over weather data call constructor for building object from data.
-        const weatherArray = cityWeather.data.map(day => new Forecast(day));
-
-
-        response.status(200).send(weatherArray);
-
-};
-
-
-// app.get('/weather', (request, response) => {
-//     //this gives us the city to map over the weather json object
-//     response.send({ express: request.query.city });
-//     let searchQuery =  request.query.city;
-//     console.log(`search query is: ${searchQuery}`);
-    
-
-//     const cityWeather = weatherData.find(city => city.city_name.toLowerCase() === searchQuery.toLowerCase());
-//     console.log('the variable city =', cityWeather);
-    
-//     const weatherArray = weatherData.map(day => new Forecast(day));
-//     // response.status(200).send(weatherArray);
-//     // console.log(description);
-    
-// });
-
+	response.status(200).send(weatherArray);
+}
 
 class Forecast {
-
-    constructor(day) {
-        console.log('day constructor', day.datetime)
-        console.log('description constructor', day.weather.description)
-        this.date = day.datetime;
-        this.description = day.weather.description;
-    };
-};
+	constructor(day) {
+		console.log('day constructor', day.datetime);
+		console.log('description constructor', day.weather.description);
+		this.date = day.datetime;
+		this.description = day.weather.description;
+	}
+}
 
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+	console.log(`Server listening on ${PORT}`);
 });
-
