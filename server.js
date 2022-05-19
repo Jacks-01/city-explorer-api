9fd53899837bb708b2949b102f2f03de04870d48
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-05-19 10:08:06 -0500

    lab 7 backend done

--- server.js
+++ server.js
@@ -4,41 +4,58 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
-const PORT = process.env.PORT || 5000;
-let data = require('./data/weather.json');
+const PORT = process.env.PORT || 5001;
+let weatherData = require('./data/weather.json');
 
-class Forecast {
+app.get('/weather', handleWeather);
 
-    constructor(date,description) {
-        this.date = date;
-        this.description = description;
-    };
+
+function handleWeather(request, response){
+    console.log('server side ',request.query);
+        let searchQuery = request.query.city;
+        console.log('server console.log', searchQuery);
+
+        //get weather data
+        const cityWeather = weatherData.find(city => city.city_name.toLowerCase() === searchQuery.toLowerCase());
+        console.log('cityWeather', cityWeather.data);
+
+        //loop over weather data call constructor for building object from data.
+        const weatherArray = cityWeather.data.map(day => new Forecast(day));
+
+
+        response.status(200).send([weatherArray]);
+
 };
 
 
+// app.get('/weather', (request, response) => {
+//     //this gives us the city to map over the weather json object
+//     response.send({ express: request.query.city });
+//     let searchQuery =  request.query.city;
+//     console.log(`search query is: ${searchQuery}`);
+    
 
-function getWeather() {
-    let date = [];
-    let description = [];
+//     const cityWeather = weatherData.find(city => city.city_name.toLowerCase() === searchQuery.toLowerCase());
+//     console.log('the variable city =', cityWeather);
+    
+//     const weatherArray = weatherData.map(day => new Forecast(day));
+//     // response.status(200).send(weatherArray);
+//     // console.log(description);
+    
+// });
 
-    data.find()
 
+class Forecast {
 
-
-}
-
-
-app.get('/weather', (request, response) => {
-    response.send({ express: request.query.city });
-	// console.log({ data: request.query.city});
-	console.log({ express: request.query.city });
-});
-
-//Fallback
-app.get('/', (request, response) => {
-    response.send({express: request.query.city})
-});
+    constructor(day) {
+        console.log('day constructor', day.datetime)
+        console.log('description constructor', day.weather.description)
+        this.date = day.datetime;
+        this.description = day.weather.description;
+    };
+};
 
 app.listen(PORT, () => {
-	console.log(`Server listening on ${PORT}`);
+    console.log(`Server listening on ${PORT}`);
 });
+
