7a132d81054d64f12e896edd1d4568e61a4c8b85
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-05-20 14:18:05 -0500

    server is pulling data from weather.io correctly

--- server.js
+++ server.js
@@ -3,50 +3,55 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-// import express from 'express';
-// import cors from 'cors';
-// import dotenv from 'dotenv';
-// import axios from 'axios';
 app.use(cors());
-// import { env } from 'process';
 const PORT = process.env.PORT || 5001;
 const weatherKey = process.env.WEATHER_API_KEY;
 const axios = require('axios').default;
+
+
+
 console.log(PORT);
 console.log(weatherKey);
-axios({
-	method: 'get',
-	url: 'http://api.weatherbit.io/v2.0/forecast/daily',
-	params: {
-		city: 'Cedar Rapids',
-		key: weatherKey,
-	}})
-		.then((response) => {
-			console.log(response.data);
-		})
-		.catch((err) => {
-			console.error(err);
-		})
+
+app.get('/weather', handleWeather);
+
+function handleWeather(request, response) {
+    console.log('server side ', request.query);
+    let cityQuery = request.query.city
+    let lonQuery = request.query.lon;
+    let latQuery = request.query.lat;
+    console.log(`server has recieved the following parameters: ${cityQuery} + ${lonQuery} + ${latQuery}`);
+    axios({
+        method: 'get',
+        url: 'http://api.weatherbit.io/v2.0/forecast/daily',
+        params: {
+            city: cityQuery,
+            lat: latQuery,
+            lon: lonQuery,
+            key: weatherKey,
+        }})
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((err) => {
+                console.error(err);
+            })
+    
+    // //get weather data
+    // const cityWeather = weatherData.find(
+    //     (city) => city.city_name.toLowerCase() === searchQuery.toLowerCase()
+    // );
+    // console.log('cityWeather', cityWeather.data);
+
+    // //loop over weather data call constructor for building object from data.
+    // const weatherArray = cityWeather.data.map((day) => new Forecast(day));
+
+    // response.status(200).send(weatherArray);
+}
 
 
-// app.get('/weather', handleWeather);
 
-// function handleWeather(request, response) {
-// 	console.log('server side ', request.query);
-// 	let searchQuery = request.query.city;
-// 	console.log('server console.log', searchQuery);
 
-// 	//get weather data
-// 	const cityWeather = weatherData.find(
-// 		(city) => city.city_name.toLowerCase() === searchQuery.toLowerCase()
-// 	);
-// 	console.log('cityWeather', cityWeather.data);
-
-// 	//loop over weather data call constructor for building object from data.
-// 	const weatherArray = cityWeather.data.map((day) => new Forecast(day));
-
-// 	response.status(200).send(weatherArray);
-// }
 
 // class Forecast {
 // 	constructor(day) {
@@ -57,6 +62,6 @@ axios({
 // 	}
 // }
 
-// app.listen(PORT, () => {
-// 	console.log(`Server listening on ${PORT}`);
-// });
+app.listen(PORT, () => {
+	console.log(`Server listening on ${PORT}`);
+});
