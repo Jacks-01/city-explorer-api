3bdbd8f10e31b78ebfda1cab185dd46faab5682c
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-05-20 17:28:23 -0500

    lab 8 weather complete

--- server.js
+++ server.js
@@ -8,68 +8,63 @@ const PORT = process.env.PORT || 5001;
 const weatherKey = process.env.WEATHER_API_KEY;
 const axios = require('axios').default;
 
-
-
 console.log(PORT);
 console.log(weatherKey);
 
 app.get('/weather', handleWeather);
 
-function formatData() {
-
-    
-};
+function formatData() {}
 
 function handleWeather(request, response) {
-    console.log('server side ', request.query);
-    let cityQuery = request.query.city
-    let lonQuery = request.query.lon;
-    let latQuery = request.query.lat;
-    console.log(`server has recieved the following parameters: ${cityQuery} + ${lonQuery} + ${latQuery}`);
-    axios({
-        method: 'get',
-        url: 'http://api.weatherbit.io/v2.0/forecast/daily',
-        params: {
-            city: cityQuery,
-            lat: latQuery,
-            lon: lonQuery,
-            key: weatherKey,
-        },
-        transformResponse: [function (data) {
-            console.log('this is our data',  JSON.parse(data));
-            let betterData = JSON.parse(data);
-            let weatherArray = [];
-            for(let i = 0; i < betterData.length; i++) {
-                let description = betterData.weather.description;
-                let dateTime = betterData.datetime;
-                weatherArray.push({description: description, datetime: dateTime});
-            };
-            return weatherArray;
+	console.log('server side ', request.query);
+	let cityQuery = request.query.city;
+	let lonQuery = request.query.lon;
+	let latQuery = request.query.lat;
+	console.log(
+		`server has recieved the following parameters: ${cityQuery} + ${lonQuery} + ${latQuery}`
+	);
+	axios({
+		method: 'get',
+		url: 'http://api.weatherbit.io/v2.0/forecast/daily',
+		params: {
+			city: cityQuery,
+			lat: latQuery,
+			lon: lonQuery,
+			key: weatherKey,
+		},
+		transformResponse: [
+			function (data) {
+				// console.log('this is our data',  JSON.parse(data));
+				let betterData = JSON.parse(data);
+    				let weatherArray = [];
+				for (let i = 0; i < betterData.data.length; i++) {
+					let description = betterData.data[i].weather.description;
+					let dateTime = betterData.data[i].datetime;
+					weatherArray.push({ description: description, datetime: dateTime });
+				}
+				return weatherArray;
+			},
+		],
+	})
+		.then((res) => {
+			console.log(response.data);
+			response.send(res.data);
+		})
+		.catch((err) => {
+			console.error(err);
+		});
 
-        }],
-    })
-            .then((response) => {
-                console.log(response.data);
-            })
-            .catch((err) => {
-                console.error(err);
-            })
-    
-    // //get weather data
-    // const cityWeather = weatherData.find(
-    //     (city) => city.city_name.toLowerCase() === searchQuery.toLowerCase()
-    // );
-    // console.log('cityWeather', cityWeather.data);
+	// //get weather data
+	// const cityWeather = weatherData.find(
+	//     (city) => city.city_name.toLowerCase() === searchQuery.toLowerCase()
+	// );
+	// console.log('cityWeather', cityWeather.data);
 
-    // //loop over weather data call constructor for building object from data.
-    // const weatherArray = cityWeather.data.map((day) => new Forecast(day));
+	// //loop over weather data call constructor for building object from data.
+	// const weatherArray = cityWeather.data.map((day) => new Forecast(day));
 
-    // response.status(200).send(weatherArray);
+	// response.status(200).send(weatherArray);
 }
-
-
-
-
 
 // class Forecast {
 // 	constructor(day) {
